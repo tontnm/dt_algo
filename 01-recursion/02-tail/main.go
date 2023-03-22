8cbd2672198abdef2c42003616ce4018c4098008
tontnm <tontnm@gmail.com>
2023-03-23 00:21:23 +0700

    tail recursion

--- 01-recursion/02-tail/main.go
+++ 01-recursion/02-tail/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	m, n := 7, 3
+	result := gcd(m, n)
+	fmt.Println(result)
+}
+
+// tìm ước số chung lớn nhất
+func gcd(m, n int) int {
+	var r int
+
+	if m < n {
+		return gcd(n, m)
+	}
+
+	r = m % n
+
+	if r == 0 {
+		return n
+	} else {
+		return gcd(n, r)
+	}
 }
